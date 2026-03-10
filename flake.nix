247cd4fae1de7a8473ad8631d190f1d8a4436857
Jakob Ankarhem <jakob@ankarhem.dev>
2026-03-10 16:00:22 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "TypeScript development environment";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          treefmt = {
+            programs.nixfmt.enable = true;
+            programs.nixfmt.package = pkgs.nixfmt;
+            programs.prettier.enable = true;
+          };
+          pre-commit.settings.hooks = {
+            treefmt.enable = true;
+          };
+          devShells.default = pkgs.mkShell {
+            inherit (config.pre-commit) shellHook;
+            packages =
+              with pkgs;
+              [
+                bun
+              ]
+              ++ config.pre-commit.settings.enabledPackages;
+          };
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = lib.attrValues self.overlays;
+          };
+        };
+      flake.overlays.default = final: prev: {
+        nodejs = final.nodejs_24;
+      };
+    };
+}
